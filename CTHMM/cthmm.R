72d855bd3082f2b30235a994953c5ac063ffd926
saeedghodsi93 <saeedghodsi93@gmail.com>
2022-05-03 20:18:40 -0700

    commit

--- CTHMM/cthmm.R
+++ CTHMM/cthmm.R
@@ -1,5 +1,59 @@
 
-saeed <- function(code) {
-  print("ghodsi")
-  print(code)
+# generate data, run the em algorithm and analyze the results for the CTHMM model
+cthmm.func <- function(debugging.mode) {
+  
+  # the number of observations
+  N <- 100
+  
+  # the mean and std of the number of visits
+  H.params <- c(10,3)
+  
+  # the mean and std of the between visit time distribution
+  between.visit.time.params <- c(10,3)
+  
+  # set both the true and initial model parameters
+  ret.set.model.parameters <- set.model.parameters.func()
+  I <- ret.set.model.parameters$I
+  J <- ret.set.model.parameters$J
+  L <- ret.set.model.parameters$L
+  pi <- ret.set.model.parameters$pi
+  lambda <- ret.set.model.parameters$lambda
+  R <- ret.set.model.parameters$R
+  Q <- ret.set.model.parameters$Q
+  mu  <- ret.set.model.parameters$mu
+  eta  <- ret.set.model.parameters$eta
+  pi.tilde <- ret.set.model.parameters$pi.tilde
+  Q.tilde <- ret.set.model.parameters$Q.tilde
+  mu.tilde  <- ret.set.model.parameters$mu.tilde
+  eta.tilde  <- ret.set.model.parameters$eta.tilde
+  
+  # generate synthetic data
+  ret.data.generation <- data.generation(I, J, L, pi, lambda, R, Q, mu, eta, N, H.params, between.visit.time.params)
+  H <- ret.data.generation$H
+  a <- ret.data.generation$a
+  tau.true <- ret.data.generation$tau.true
+  tau.obs <- ret.data.generation$tau.obs
+  z.true <- ret.data.generation$z.true
+  z.obs <- ret.data.generation$z.obs
+  y.obs <- ret.data.generation$y.obs
+  u.obs <- ret.data.generation$u.obs
+  
+  # visualize the data
+  # visualization.func(H, a, tau.true, tau.obs, z.true, z.obs, y.obs, u.obs)
+  
+  # run the EM algorithm
+  counter.em.max <- 20
+  ret.EM.algorithm <- EM.algorithm.func(N, I, J, L, H, a, tau.obs, y.obs, u.obs, pi.tilde, Q.tilde, mu.tilde, eta.tilde, counter.em.max, debugging.mode)
+  pi.tilde <- ret.EM.algorithm$pi.tilde
+  Q.tilde <- ret.EM.algorithm$Q.tilde
+  mu.tilde <- ret.EM.algorithm$mu.tilde
+  eta.tilde <- ret.EM.algorithm$eta.tilde
+  
+  # print the results
+  print(pi.tilde)
+  print(Q.tilde)
+  print(mu.tilde)
+  print(eta.tilde)
+  
 }
+
--- CTHMM/forward_backward.R
+++ CTHMM/forward_backward.R
@@ -1,5 +1,125 @@
 
-saeed <- function(code) {
-  print("ghodsi")
-  print(code)
+# transition model
+transition.prob.func <- function(Q.in, i.in, k.in, l.in, tau.in) {
+  
+  transition.mat <- expm(tau.in * Q.in[, , l.in])
+  
+  ret <- transition.mat[i.in, k.in]
+  
+  return(ret)
+  
 }
+
+
+# emission model
+emission.prob.func <- function(J, mu.in, j.in) {
+  
+  ret <- choose(J-1, j.in) * (mu.in ^ j.in) * ((1-mu.in) ^ (J-1 - j.in))
+  
+  return(ret)
+  
+}
+
+
+# intervention model
+intervention.prob.func <- function(L, eta.in, l.in) {
+  
+  ret <- choose(L-1, l.in) * (eta.in ^ l.in) * ((1-eta.in) ^ (L-1 - l.in))
+  
+  return(ret)
+  
+}
+
+
+# the forward-backward algorithm for calculating the end-state posterior probabilities
+forward.backward.func <- function(I, J, L, tau.obs, y.obs, u.obs, pi.in, Q.in, mu.in, eta.in, H.in, n) {
+  
+  # forward probabilities
+  alpha <- array(numeric(), c(H.in,I))
+  for (i in 1:I) {
+    alpha[1,i] <- pi.in[i] * emission.prob.func(J, mu.in[i], y.obs[n,1]) * intervention.prob.func(L, eta.in[y.obs[n,1]+1], u.obs[n,1])
+  }
+  for (t in 1:(H.in-1)) {
+    for (i in 1:I) {
+      alpha[t+1,i] <- 0
+      for (k in 1:I) {
+        alpha[t+1,i] <- alpha[t+1,i] + transition.prob.func(Q.in, k, i, u.obs[n,t]+1, tau.obs[n,t+1]-tau.obs[n,t]) * emission.prob.func(J, mu.in[i], y.obs[n,t+1]) * intervention.prob.func(L, eta.in[y.obs[n,t+1]+1], u.obs[n,t+1]) * alpha[t,k]
+      }
+    }
+  }
+  
+  # backward probabilities
+  beta <- array(numeric(), c(H.in-1,I))
+  for (i in 1:I) {
+    beta[H.in-1,i] <- 0
+    for (k in 1:I) {
+      beta[H.in-1,i] <- beta[H.in-1,i] + transition.prob.func(Q.in, i, k, u.obs[n,H.in-1]+1, tau.obs[n,H.in]-tau.obs[n,H.in-1]) * emission.prob.func(J, mu.in[k], y.obs[n,H.in]) * intervention.prob.func(L, eta.in[y.obs[n,H.in]+1], u.obs[n,H.in])
+    }
+  }
+  for (t in (H.in-1):2) {
+    for (i in 1:I) {
+      beta[t-1,i] <- 0
+      for (k in 1:I) {
+        beta[t-1,i] <- beta[t-1,i] + transition.prob.func(Q.in, i, k, u.obs[n,t-1]+1, tau.obs[n,t]-tau.obs[n,t-1]) * emission.prob.func(J, mu.in[k], y.obs[n,t]) * intervention.prob.func(L, eta.in[y.obs[n,t]+1], u.obs[n,t]) * beta[t,k]
+      } 
+    }
+  }
+  
+  # posterior probability of z_t
+  gamma <- array(numeric(), c(H.in,I))
+  for (i in 1:I) {
+    gamma[H.in,i] <- alpha[H.in,i] / sum(alpha[H.in,])
+  }
+  for (t in 1:(H.in-1)) {
+    temp <- array(numeric(), c(I))
+    for (i in 1:I) {
+      temp[i] <- alpha[t,i] * beta[t,i]
+    }
+    for (i in 1:I) {
+      gamma[t,i] <- temp[i] / sum(temp)
+    }
+  }
+  
+  # posterior probability of (z_t, z_{t+1})
+  nu <- array(numeric(), c(H.in-1,I,I))
+  temp <- array(numeric(), c(I,I))
+  for (i in 1:I) {
+    for (k in 1:I) {
+      temp[i,k] <- transition.prob.func(Q.in, i, k, u.obs[n,H.in-1]+1, tau.obs[n,H.in]-tau.obs[n,H.in-1]) * emission.prob.func(J, mu.in[k], y.obs[n,H.in]) * intervention.prob.func(L, eta.in[y.obs[n,H.in]+1], u.obs[n,H.in]) * alpha[H.in-1,i]
+    }
+  }
+  for (i in 1:I) {
+    for (k in 1:I) {
+      nu[H.in-1,i,k] <- temp[i,k] / sum(temp)
+    }
+  }
+  for (t in 1:(H.in-2)) {
+    temp <- array(numeric(), c(I,I))
+    for (i in 1:I) {
+      for (k in 1:I) {
+        temp[i,k] <- transition.prob.func(Q.in, i, k, u.obs[n,t]+1, tau.obs[n,t+1]-tau.obs[n,t]) * emission.prob.func(J, mu.in[k], y.obs[n,t+1]) * intervention.prob.func(L, eta.in[y.obs[n,t+1]+1], u.obs[n,t+1]) * alpha[t,i] * beta[t+1,k]
+      }
+    }
+    for (i in 1:I) {
+      for (k in 1:I) {
+        nu[t,i,k] <- temp[i,k] / sum(temp)
+      }
+    }
+  }
+
+  # for debugging  
+  # if (n == 100) {
+  #   print(alpha)
+  #   print(beta)
+  #   print(gamma)
+  #   print(aperm(nu, c(2,3,1)))
+  #   print(z.obs[n,])
+  #   print(y.obs[n,])
+  # }
+  
+  ret <- list("alpha" = alpha, "beta" = beta, "gamma" = gamma, "nu" = nu)
+  
+  return(ret)
+  
+}
+
